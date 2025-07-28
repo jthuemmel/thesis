177a4b73db639f9be52171e1901775f9a96dda3c
jthuemmel <jannik.thuemmel@posteo.net>
2025-07-28 15:27:52 +0200

    refactored

--- utils/networks.py
+++ utils/networks.py
@@ -1,10 +1,11 @@
-from torch.nn import Embedding, Linear, Module, ModuleList
-from torch import cat, split, Tensor, LongTensor, einsum
+from torch.nn import Embedding, Linear, Module, ModuleList, init
+from torch import cat, split, Tensor, LongTensor, zeros_like
 from dataclasses import dataclass
 from einops import repeat, rearrange, reduce
-from utils.components import TransformerBlock, ConditionalLayerNorm, GatedFFN, Interface
+from utils.components import *
 from typing import Tuple, Optional
 
+####
 class ViT(Module):
     def __init__(self, cfg: dataclass):
         """
@@ -45,11 +46,13 @@ class ViT(Module):
         x = self.proj_out(x)
         return x
 
+####
 class InterfaceNetwork(Module):
     def __init__(self, cfg: dataclass):
         super().__init__()
         # In/Out
         self.proj_in = Linear(cfg.dim_in, cfg.dim)
+        self.norm_in = ConditionalLayerNorm(cfg.dim, cfg.dim_noise)
         self.proj_out = Linear(cfg.dim, cfg.dim_out)
 
         # Embeddings
@@ -63,7 +66,8 @@ class InterfaceNetwork(Module):
 
     def forward(self, x: Tensor, pos: LongTensor, ctx: Optional[Tensor] = None):  
         # initialize interface
-        x = self.proj_in(x) + self.positions(pos)
+        x = self.proj_in(x) 
+        x = self.norm_in(x, ctx = ctx) + self.positions(pos)
         # initialize latents
         z = repeat(self.latents.weight, "z d -> b z d", b = x.size(0))      
         # update
@@ -73,50 +77,50 @@ class InterfaceNetwork(Module):
         x = self.proj_out(x)
         return x
     
-class ModalEncoder(Module):
-    def __init__(self, cfg: dataclass):
-        super().__init__()
-        self.in_projection = Embedding(cfg.num_features, cfg.dim * cfg.dim_in)
-        self.feature_bias = Embedding(cfg.num_features, cfg.dim)
-        #self.queries = Embedding(1, cfg.dim)       
-        #self.kv_norm = ConditionalLayerNorm(cfg.dim, cfg.dim_noise)
-        #self.cross_attn = TransformerBlock(cfg.dim, dim_ctx=cfg.dim_noise)
+####
+ARCHITECTURES = {
+    "interface": InterfaceNetwork,
+    "vit": ViT,
+}
 
-    def forward(self, x: Tensor, idx: LongTensor, ctx: Optional[Tensor] = None):
-        # ensure correct shapes
-        B, N, _, I = x.size()
-        # get dynamic weights 
-        w = self.in_projection(idx)
-        w = rearrange(w, 'b f (d i) -> b f d i', i = I)
-        b = self.feature_bias(idx)
-        b = rearrange(b, "b f d -> b () f d")
-        # linear projection
-        kv = einsum('b n f i, b f d i -> b n f d', x, w) + b
-        q = reduce(kv, 'b n f d -> b n d', "sum")
-        # expand query and context vectors
-        #q = repeat(self.queries.weight, 'q d -> b n q d', b = B, n = N)
-        #ctx = rearrange(ctx, 'b 1 d -> b 1 1 d') if ctx is not None else None
-        # normalize and add feature-bias
-        #kv = self.kv_norm(kv, ctx) + b
-        # cross attend
-        #q = self.cross_attn(q = q, kv = kv, ctx = ctx).squeeze(2)
-        return q
-    
-class ModalDecoder(Module):
-    def __init__(self, cfg: dataclass):
+####
+class MTM(Module):
+    def __init__(self, decoder_cfg: dataclass, encoder_cfg: Optional[dataclass] = None):
         super().__init__()
-        #self.norm = ConditionalLayerNorm(cfg.dim, cfg.dim_noise)
-        #self.ffn = GatedFFN(cfg.dim)
-        self.feature_bias = Embedding(cfg.num_features, cfg.dim)
-        self.out_projection = Embedding(cfg.num_features, cfg.dim * cfg.dim_out)
+        self.queries = Embedding(1, decoder_cfg.dim)
+        self.encoder = ARCHITECTURES[encoder_cfg.architecture](encoder_cfg) if encoder_cfg else None
+        self.decoder = ARCHITECTURES[decoder_cfg.architecture](decoder_cfg)
 
-    def forward(self, x: Tensor, idx: LongTensor, ctx: Optional[Tensor] = None):
-        _, _, D = x.size()
-        #x = self.ffn(self.norm(x, ctx))
-        w = self.out_projection(idx)
-        w = rearrange(w, "b f (d o) -> b f d o", d = D)
-        x = rearrange(x, 'b n d -> b n 1 d')
-        b = self.feature_bias(idx)
-        b = rearrange(b, 'b f d -> b 1 f d')
-        out = einsum("b n f d, b f d o -> b n f o", x + b, w)
-        return out
+        self.apply(self.base_init)
+        self.apply(self.zero_init)
+
+    @staticmethod
+    def base_init(m):
+        if isinstance(m, Linear):
+            init.trunc_normal_(m.weight, std = 0.02)
+        if isinstance(m, Embedding):
+            init.trunc_normal_(m.weight, std = 0.02)
+
+    @staticmethod
+    def zero_init(m):
+        if isinstance(m, Attention):
+            init.zeros_(m.to_out.weight)
+        if isinstance(m, GatedFFN):
+            init.zeros_(m.to_out.weight)
+        if isinstance(m, ConditionalLayerNorm):
+            init.zeros_(m.linear.weight)
+
+    def forward(self, 
+                src: Tensor, 
+                pos_src: LongTensor, 
+                pos_tgt: LongTensor, 
+                ctx: Optional[Tensor] = None
+                ) -> Tuple[Tensor, Tensor]:
+        if self.encoder is not None:
+            src = self.encoder(src, pos_src, ctx = ctx)
+        q = self.queries(zeros_like(pos_tgt))
+        x_hat = cat([src, q], dim=1)
+        pos = cat([pos_src, pos_tgt], dim=1)
+        x_hat = self.decoder(x_hat, pos, ctx = ctx)
+        src, tgt = x_hat.split([pos_src.size(1), pos_tgt.size(1)], dim=1)
+        return src, tgt
