753aa4d397ddb96c397d17f0616b3e754a6e5c98
jthuemmel <jannik.thuemmel@posteo.net>
2025-08-04 21:00:31 +0200

    cpe version

--- utils/field_network.py
+++ utils/field_network.py
@@ -13,8 +13,9 @@ class StochasticWeatherField(Module):
         self.latent_embedding = Embedding(cfg.num_latents, cfg.dim)
 
         # Coordinates
-        self.coordinates = ContinuousPositionalEmbedding(dim_per_coord=cfg.dim_coords, max_positions=[6, cfg.num_features, 16, 30], model_dim=cfg.dim)        
-
+        self.src_positions = ContinuousPositionalEmbedding(dim_per_coord=cfg.dim_coords, max_positions=[6, cfg.num_features, 16, 30], model_dim=cfg.dim)        
+        self.tgt_positions = ContinuousPositionalEmbedding(dim_per_coord=cfg.dim_coords, max_positions=[6, cfg.num_features, 16, 30], model_dim=cfg.dim)
+        
         # I/O
         self.proj_in = Linear(cfg.dim_in, cfg.dim)
         self.norm_in = ConditionalLayerNorm(cfg.dim)
@@ -33,6 +34,8 @@ class StochasticWeatherField(Module):
             Interface(cfg.dim, cfg.num_compute_blocks, dim_heads= cfg.dim_heads, dim_ctx=cfg.dim_noise) 
             for _ in range(cfg.num_layers)
         ])
+
+        self.decoder = Interface(cfg.dim, cfg.num_compute_blocks, dim_heads= cfg.dim_heads, dim_ctx=cfg.dim_noise)
 
         # Initialization
         self.apply(self.base_init)
@@ -93,23 +96,26 @@ class StochasticWeatherField(Module):
             ):
         # Initialize src tokens and add positional embeddings
         src = self.proj_in(src)
-        src = self.norm_in(src) + self.coordinates(src_coords)
+        src = self.norm_in(src) + self.src_positions(src_coords)
 
         # Initialize latent tokens with self conditioning
         z_init = repeat(self.latent_embedding.weight, "z d -> b z d", b = src.size(0))
         z = self.latent_conditioning(z_init, z_prev) 
 
         # Initialize query tokens with self conditioning
-        q_init = self.coordinates(tgt_coords)
+        q_init = self.tgt_positions(tgt_coords)
         q = self.query_conditioning(q_init, q_prev)
 
         # Shared noise projection
         noise = self.proj_noise(noise) if noise is not None else None
 
-        # Interface network computation over src and query
-        x = torch.cat([src, q], dim = 1)
+        # Interface network computation over src
         for block in self.network:
-            x, z = block(x, z, noise)
+            src, z = block(src, z, noise)
+
+        # Interface network over src and query
+        x = torch.cat([src, q], dim = 1)
+        x, z = self.decoder(x, z, noise)
         _, q = x.split([src_coords.size(1), tgt_coords.size(1)], dim = 1)
 
         # Decode query only
