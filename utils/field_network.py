b7f8ebfc94d7dac210fedd45dd6a668b1e315647
jthuemmel <jannik.thuemmel@posteo.net>
2025-08-05 17:52:33 +0200

    refactored to latent noise injection and CPE

--- utils/field_network.py
+++ utils/field_network.py
@@ -3,7 +3,7 @@ from torch.nn import Embedding, Module, ModuleList, Linear, init, LayerNorm
 from einops import rearrange, repeat, reduce
 from dataclasses import dataclass
 from utils.networks import Interface
-#from utils.cpe import ContinuousPositionalEmbedding
+from utils.cpe import ContinuousPositionalEmbedding
 from utils.components import *
 
 class StochasticWeatherField(Module):
@@ -13,15 +13,11 @@ class StochasticWeatherField(Module):
         self.latent_embedding = Embedding(cfg.num_latents, cfg.dim)
 
         # Coordinates
-        #self.src_positions = ContinuousPositionalEmbedding(dim_per_coord=cfg.dim_coords, max_positions=[6, cfg.num_features, 16, 30], model_dim=cfg.dim)        
-        #self.tgt_positions = ContinuousPositionalEmbedding(dim_per_coord=cfg.dim_coords, max_positions=[6, cfg.num_features, 16, 30], model_dim=cfg.dim)
-        
-        self.coords = Embedding(cfg.num_tokens, cfg.dim_coords)
-        self.to_src_coords = Linear(cfg.dim_coords, cfg.dim)
-        self.to_tgt_coords = Linear(cfg.dim_coords, cfg.dim)
+        self.coords = ContinuousPositionalEmbedding(cfg.dim_coords, [8, 8, 16, 32], cfg.dim)
+        self.query_ffn = GatedFFN(cfg.dim)
 
         # I/O
-        self.proj_in = Linear(cfg.dim_in, cfg.dim)
+        self.proj_in = Embedding(cfg.num_features, cfg.dim * cfg.dim_in)
         self.norm_in = ConditionalLayerNorm(cfg.dim)
         self.proj_out = Linear(cfg.dim, cfg.dim_out)
 
@@ -30,16 +26,15 @@ class StochasticWeatherField(Module):
         self.latent_conditioning = ConditioningNetwork(cfg.dim)
 
         # Noise projection
-        self.proj_noise = Linear(cfg.dim_noise, cfg.dim_noise) if cfg.dim_noise is not None else None
+        self.proj_noise = Linear(cfg.dim_noise, cfg.dim) if cfg.dim_noise is not None else None
         self.dim_noise = cfg.dim_noise
+        self.generator = None
 
         # Interface Network
         self.network = ModuleList([
-            Interface(cfg.dim, cfg.num_compute_blocks, dim_heads= cfg.dim_heads, dim_ctx=cfg.dim_noise) 
+            Interface(cfg.dim, cfg.num_compute_blocks, dim_heads= cfg.dim_heads, dim_ctx=None) 
             for _ in range(cfg.num_layers)
         ])
-
-        self.decoder = Interface(cfg.dim, cfg.num_compute_blocks, dim_heads= cfg.dim_heads, dim_ctx=cfg.dim_noise)
 
         # Initialization
         self.apply(self.base_init)
@@ -70,11 +65,6 @@ class StochasticWeatherField(Module):
             torch.nn.init.trunc_normal_(m.to_out.weight, std = 1e-8)
         if isinstance(m, GatedFFN):
             torch.nn.init.trunc_normal_(m.to_out.weight, std = 1e-8)
-        
-    def noise_like(self, src: torch.Tensor):
-        if self.dim_noise is not None:
-            return torch.randn(src.size(0), 1, self.dim_noise, device=src.device, dtype=src.dtype)
-        return None
 
     def forward(self, 
             src: torch.Tensor, 
@@ -84,11 +74,38 @@ class StochasticWeatherField(Module):
             ):
         q, z = None, None
         for _ in range(num_steps):
-            noise = self.noise_like(src)
-            pred, q, z = self.step(src, src_coords, tgt_coords, q_prev=q, z_prev=z, noise=noise)
+            pred, q, z = self.step(src, src_coords, tgt_coords, q_prev=q, z_prev=z)
             q = q.detach()
             z = z.detach()
         return pred
+    
+    def noise_like(self, src: torch.Tensor):
+        if self.dim_noise is not None:
+            return torch.randn(src.size(0), 1, self.dim_noise, device=src.device, dtype=src.dtype, generator = self.generator)
+        return None
+    
+    def to_x(self, src, src_coords):
+        src_var = src_coords[:, :, 1] # variable index from [B, N, C]
+        w = self.proj_in(src_var)
+        w = rearrange(w, "b n (d i) -> b n d i", i = src.size(-1))
+        x = torch.einsum("b n i, b n d i -> b n d", src, w)
+        x = self.norm_in(x) + self.coords(src_coords)
+        return x
+    
+    def to_q(self, tgt_coords, q_prev):
+        q_init = self.coords(tgt_coords)
+        q_init = q_init + self.query_ffn(q_init)
+        q = self.query_conditioning(q_init, q_prev)
+        return q
+               
+    def to_z(self, src, z_prev):
+        z_init = repeat(self.latent_embedding.weight, "z d -> b z d", b = src.size(0))
+        if self.dim_noise is not None:
+            noise = self.noise_like(src)
+            z_noise = self.proj_noise(noise)
+            z_init = torch.cat([z_init, z_noise], dim = 1)
+        z = self.latent_conditioning(z_init, z_prev)
+        return z
 
     def step(self, 
             src: torch.Tensor, 
@@ -96,35 +113,17 @@ class StochasticWeatherField(Module):
             tgt_coords: torch.Tensor,
             q_prev: torch.Tensor = None,
             z_prev: torch.Tensor = None,
-            noise: torch.Tensor = None
             ):
-        # Embed the coords
-        src_coords = self.coords(src_coords)
-        tgt_coords = self.coords(tgt_coords)
-
-        # Initialize src tokens and add positional embeddings
-        src = self.proj_in(src)
-        src = self.norm_in(src) + self.to_src_coords(src_coords)
-
-        # Initialize latent tokens with self conditioning
-        z_init = repeat(self.latent_embedding.weight, "z d -> b z d", b = src.size(0))
-        z = self.latent_conditioning(z_init, z_prev) 
-
-        # Initialize query tokens with self conditioning
-        q_init = self.to_tgt_coords(tgt_coords)
-        q = self.query_conditioning(q_init, q_prev)
-
-        # Shared noise projection
-        noise = self.proj_noise(noise) if noise is not None else None
-
-        # Interface network computation over src
-        for block in self.network:
-            src, z = block(src, z, noise)
-
+        
+        x = self.to_x(src, src_coords)
+        q = self.to_q(tgt_coords, q_prev)
+        z = self.to_z(src, z_prev)
+        
         # Interface network over src and query
-        x = torch.cat([src, q], dim = 1)
-        x, z = self.decoder(x, z, noise)
-        _, q = x.split([src_coords.size(1), tgt_coords.size(1)], dim = 1)
+        combined = torch.cat([x, q], dim = 1)
+        for block in self.network:
+            combined, z = block(combined, z)
+        _, q = combined.split([src_coords.size(1), tgt_coords.size(1)], dim = 1)
 
         # Decode query only
         pred = self.proj_out(q)
