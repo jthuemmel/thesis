5888e60490cf06c932ae4330afcb5f743d979aa5
jthuemmel <jannik.thuemmel@posteo.net>
2025-09-15 14:26:49 +0200

    reordering for easier copy paste

--- utils/index_linear.py
+++ utils/index_linear.py
@@ -15,26 +15,6 @@ class IndexLinear(torch.nn.Module):
         b = None if self.bias is None else self.bias(idx)
         return torch.nn.functional.linear(x, W, b)
 
-class GroupLinear(torch.nn.Module):
-    def __init__(self, dim_in: int, dim_out: int, num_groups: int, bias: bool = True):
-        super().__init__()
-        self.linear = IndexLinear(dim_in, dim_out, num_groups, bias)
-
-    def forward(self, x: torch.Tensor, group_by: torch.LongTensor):
-        # pre-allocate output tensor
-        out = x.new_empty(*x.shape[:-1], self.linear.dim_out)
-        # flat views
-        xf = rearrange(x, '... d -> (...) d')
-        of = rearrange(out, '... d -> (...) d')
-        gf = rearrange(group_by, '... -> (...)')
-        # determine which groups are present
-        groups = gf.unique(sorted = False)
-        # apply linear to groups
-        for i, s in enumerate(groups):
-            idx = (gf == s).nonzero().squeeze(1) # find all elements of the group
-            of[idx] = self.linear(xf[idx], i)
-        return out
-
 class SegmentLinear(torch.nn.Module):
     def __init__(self, dim_in: int, dim_out: int, indices: torch.LongTensor, bias: bool = True):
         super().__init__()
@@ -57,3 +37,24 @@ class SegmentLinear(torch.nn.Module):
 
     def forward(self, x: torch.Tensor):
         return torch.cat([self.linear(x[:, s, :], idx) for idx, s in self.segments], dim = 1)
+
+class GroupLinear(torch.nn.Module):
+    def __init__(self, dim_in: int, dim_out: int, num_groups: int, bias: bool = True):
+        super().__init__()
+        self.linear = IndexLinear(dim_in, dim_out, num_groups, bias)
+
+    def forward(self, x: torch.Tensor, group_by: torch.LongTensor):
+        # pre-allocate output tensor
+        out = x.new_empty(*x.shape[:-1], self.linear.dim_out)
+        # flat views
+        xf = rearrange(x, '... d -> (...) d')
+        of = rearrange(out, '... d -> (...) d')
+        gf = rearrange(group_by, '... -> (...)')
+        # determine which groups are present
+        groups = gf.unique(sorted = False)
+        # apply linear to groups
+        for i, s in enumerate(groups):
+            idx = (gf == s).nonzero().squeeze(1) # find all elements of the group
+            of[idx] = self.linear(xf[idx], i)
+        return out
+
