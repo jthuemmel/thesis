902379858aabf0b9246adee1c449be2d2e6dbf60
jthuemmel <jannik.thuemmel@posteo.net>
2025-09-23 10:00:05 +0200

    land sea mask in until I figure out how to make xarray ignore it

--- experiment.py
+++ experiment.py
@@ -204,6 +204,7 @@ class Experiment(DistributedTrainer):
             mask= 'bernoulli',
         )
         prediction = self.model(tokens, visible) if self.mode == 'train' or not self.cfg.use_ema else self.ema_model(tokens, visible)
+        prediction = prediction * self.land_sea_mask[..., None]
         prediction = self.tokens_to_field(prediction)
         return prediction
 
@@ -215,6 +216,7 @@ class Experiment(DistributedTrainer):
             mask= self.world.mask,
         )
         prediction = self.model(tokens, visible) if self.mode == 'train' or not self.cfg.use_ema else self.ema_model(tokens, visible)
+        prediction = prediction * self.land_sea_mask[..., None]
         loss = self.loss_fn(prediction, tokens, visible, weight)
         metrics = self.compute_metrics(ens = prediction.detach(), obs = tokens, vis = visible)
         metrics['loss'] = loss.item()
@@ -257,7 +259,6 @@ class Experiment(DistributedTrainer):
         #meta data
         meta_data = self.val_dataset.dataset
         time, lat, lon = meta_data.time, meta_data.lat, meta_data.lon
-        lsm = self.get_xr_lsm(meta_data)
         ens = np.arange(pred.shape[-1])
         tau = self.world.tau
         T, tt = self.world.token_sizes["t"], self.world.patch_sizes["tt"]
@@ -291,7 +292,6 @@ class Experiment(DistributedTrainer):
             )
             arrays.append(data_array)
         ds = xr.merge(arrays)
-        ds = ds * lsm
         return ds
 
     def get_xr_lsm(self, data: xr.Dataset):
