5f38133755b76ba7eaa5ccda2ee6abd48e33d57d
jthuemmel <jannik.thuemmel@posteo.net>
2025-10-10 16:17:55 +0200

    minimal version for future work

--- utils/objective.py
+++ utils/objective.py
@@ -1,34 +1,39 @@
 import torch
 import einops
-
-from utils.components import exists
 from utils.model import MaskedPredictor
 
 class MaskedDiffusion(torch.nn.Module):
     def __init__(self, model, world, generator: torch.Generator = None):
         super().__init__()
-
         # Generator
         self.generator = generator
         
         # Denoiser
         self.model = MaskedPredictor(model, world)
+            
+    def forward(self, x0: torch.FloatTensor, schedule: list, E: int, lsm: torch.BoolTensor):
+        out = []
+        z = None
+        x = einops.repeat(x0, 'b ... -> (b e) ...', e = E)
+        lsm = einops.repeat(lsm, 'b ... -> (b e) ...', e = E)
 
-        # 
+        for s in schedule:
+            noise = torch.randn((x.size(0), 1, self.model.dim_noise), device = x.device, generator = self.generator)
 
-    def noise_like(self, x):
-         return torch.randn((x.size(0), 1, self.model.dim_noise), device = x.device, generator = self.generator)
-    
-    @staticmethod
-    def repeat_as_ensemble(arg, E: int):
-        return einops.repeat(arg, 'b ... -> (b e) ...', e = E)
-    
-    @staticmethod
-    def batch_to_ensemble(arg, E: int):
-         return einops.rearrange(arg, '(b e) ... -> b ... e', e = E)
+            m = torch.bernoulli(1 - s, generator=self.generator).bool() # s is visibility rate
+            m = einops.repeat(m, 'b ... -> (b e) ...', e = E)
 
+            xs, zs = self.model(tokens = x, mask = m, latents = z, noise = noise)
+            
+            fm = torch.logical_and(m, lsm) #masked and sea
+            xs = torch.where(fm, xs, x) # copy unmasked and land
 
-    def forward(self, x0, S: int, E: int):
+            out.append((
+                einops.rearrange(xs, '(b e) ... -> b ... e', e = E),
+                einops.rearrange(fm, '(b e) ... -> b ... e', e = E)
+                ))
 
-
-        return             
+            z = zs.detach()
+            x = xs.detach()
+        
+        return out
