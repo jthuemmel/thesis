9dc69cbac96316c0a39037375570e7a0a3671ff6
jthuemmel <jannik.thuemmel@posteo.net>
2025-10-30 13:26:24 +0100

    testing now

--- utils/objective.py
+++ utils/objective.py
@@ -3,7 +3,7 @@ import einops
 from utils.model import MaskedPredictor
 
 class AnyOrder_RIN(torch.nn.Module):
-    def __init__(self, model, world, device: torch.Device, generator: torch.Generator = None):
+    def __init__(self, model, world, device: torch.device, generator: torch.Generator = None):
         super().__init__()
         # Generator and device
         self.generator = generator
@@ -34,57 +34,76 @@ class AnyOrder_RIN(torch.nn.Module):
         mask.scatter_(dim = -1, index = idx, src= ks > rank) # True for top-k False otherwise
         return mask
 
-    @staticmethod
-    def rate_to_k(r: torch.FloatTensor, N: int) -> torch.LongTensor:
-        return (r * N).long()
-
     # SCHEDULES
     @staticmethod
     def cosine_schedule(t: torch.FloatTensor) -> torch.FloatTensor:
-        return 1 - torch.cos(torch.pi * t / 2)
+        rates = 1 - torch.cos(torch.pi * t / 2)
+        weights = 0.5 * torch.pi * torch.sin(torch.pi * t / 2)
+        return rates, weights
 
     @staticmethod
-    def cosine_dt(t: torch.FloatTensor) -> torch.FloatTensor:
-        return 0.5 * torch.pi * torch.sin(torch.pi * t / 2)
+    def linear_schedule(t: torch.FloatTensor) -> torch.FloatTensor:
+        rates = t
+        weights = torch.ones_like(t)
+        return rates, weights
+    
+    @staticmethod
+    def minmax_schedule(t: torch.FloatTensor, a: float = 0., b: float = 1.) -> torch.FloatTensor:
+        rates = a + (b - a) * t
+        weights = torch.ones_like(t)
+        return rates, weights
 
     # Forward
     def sample_permutation(self, prior: str):
-        if prior == 'dirichlet':
+        if prior == 'dirichlet': # weight frames via dirichlet prior
             W = self.log_dirichlet((self.world.batch_size, self.world.token_sizes['t']), alpha=0.5)
             lhs = "b t"
-        elif prior == 'history':
+        elif prior == 'prefix': # ensure initial tau frames are always masked last
             W = torch.zeros((self.world.batch_size, self.world.token_sizes['t']), device=self.device)
-            W[:, :self.world.tau] = float('-inf') # ensure these are always masked last
+            W[:, :self.world.tau] = float('-inf')
             lhs = "b t"
-        else:
+        else: # uniform prior over all permutations
             W = torch.zeros((self.world.batch_size, self.world.token_sizes['t']), device=self.device)
             lhs = "b t"
         W = einops.repeat(W, f'{lhs} -> b {self.world.flat_token_pattern}', **self.world.token_sizes, b = self.world.batch_size)
         G = self.gumbel((self.world.batch_size, self.world.num_tokens))
         return G + W
     
-    def sample_masks(self, num_steps: int, prior: int, schedule: str):
-        # determine permutation order
-        prior = self.sample_permutation(prior = prior)
-        prior = einops.repeat(prior, 'b n -> s b n', s = num_steps)
-        # sample random timesteps for the whole sequence and all batch elements
-        t = torch.rand((num_steps, self.world.batch_size, 1), generator = self.generator, device = self.device)
-        # optionally, we can apply stratification here without breaking the prior
+    def sample_timesteps(self, num_steps: int, schedule: str, stratify: bool = False):
+        # sample uniform random timesteps for all steps and batch elements
+        t = self.uniform((num_steps, self.world.batch_size, 1))
+        # optionally, we can apply batch stratification here
+        if stratify:
+            t = (torch.linspace(0, 1, self.world.batch_size)[None, :, None] + t) % 1
         # sort the sequence dimension s.t. earlier steps have higher masking rates
         t = t.sort(dim = 0).values
-        # calculate k and w
-        ks = (self.cosine_schedule(t) * self.world.num_tokens).long() if schedule == "cosine" else (t * self.world.num_tokens).long()
-        ws = self.cosine_dt(t) if schedule == "cosine" else torch.ones_like(t)
-        # determine masks
-        masks = self.binary_topk(prior, ks)
-        return masks, ws
+        # calculate rs and ws based on schedule
+        if schedule == "cosine":
+            rs, ws = self.cosine_schedule(t)
+        elif schedule == 'minmax':
+            rs, ws = self.minmax_schedule(t)
+        else:
+            rs, ws = self.linear_schedule(t)
+        return rs, ws
 
-    def sample_functional(self):
+    def sample_masks(self, num_steps: int, prior: str, schedule: str, stratify: bool = False):
+        # determine permutation order
+        prior = self.sample_permutation(prior = prior)
+        # share prior across steps
+        prior = einops.repeat(prior, 'b n -> s b n', s = num_steps)
+        # sample random masking rates
+        rates, weights = self.sample_timesteps(num_steps, schedule = schedule, stratify = stratify)
+        # determine masks via gumbel-topk
+        ks = (rates * self.world.num_tokens).long()
+        masks = self.binary_topk(prior, ks)
+        return masks, weights
+
+    def sample_latent_noise(self): # sample standard normal noise for latents
         return torch.randn((self.world.batch_size, self.world.num_tokens, self.model.dim_noise), device = self.device, generator = self.generator)
 
-    def forward(self, tokens, num_steps: int = 1, num_ensemble: int = 1) -> torch.FloatTensor:
+    def forward(self, tokens, num_steps: int = 1, num_ensemble: int = 1, prior: str = 'dirichlet', schedule: str = 'cosine') -> torch.FloatTensor:
         # sample sequence of masks
-        masks, weights = self.sample_masks(num_steps)
+        masks, weights = self.sample_masks(num_steps, prior = prior, schedule = schedule)
 
         # repeat for functional risk minimization
         tokens = einops.repeat(tokens, "b n d -> (b e) n d", e = num_ensemble)
@@ -94,14 +113,14 @@ class AnyOrder_RIN(torch.nn.Module):
         latents = None
         with torch.no_grad():
             for s in range(num_steps - 1):
-                noise = self.sample_functional()
+                noise = self.sample_latent_noise()
                 tokens, latents = self.model(tokens = tokens, mask = masks[s], latents = latents, noise = noise)
                 
         # last step with gradient
-        noise = self.sample_functional()
+        noise = self.sample_latent_noise()
         tokens, latents = self.model(tokens = tokens, mask = masks[-1], latents = latents, noise = noise)
 
         # rearrange to ensemble form
         tokens = einops.rearrange(tokens, "(b e) n d -> b n d e", e = num_ensemble)
-        masks = einops.rearrange(masks, 's (b e) n d -> s b n d e', e = num_ensemble)
+        masks = einops.rearrange(masks[-1], '(b e) n d -> b n d e', e = num_ensemble)
         return tokens, masks, weights
