e66680e39e3bd424216d3db00fecf845bc791ee5
jthuemmel <jannik.thuemmel@posteo.net>
2026-01-14 00:27:46 +0100

    ?

--- utils/einmask.py
+++ utils/einmask.py
@@ -13,18 +13,16 @@ class EinMask(torch.nn.Module):
         self.network = network
         self.world = world
 
-        # pre-compute positional embedding
-        self.cpe = ContinuousPositionalEmbedding(network.dim_coords, network.wavelengths, model_dim= None)
+        # initialize coordinate grid
         idcs = torch.unravel_index(indices = torch.arange(world.num_tokens), shape = world.token_shape)
         idcs = torch.stack(idcs, dim = -1)
-        pos = self.cpe(idcs)
-        self.register_buffer('coordinates', pos)
+        self.register_buffer('coordinates', idcs)
 
         # I/O
         self.to_tokens = EinMix(
             pattern=f"{world.field_pattern} -> b {world.flat_token_pattern} d", 
             weight_shape=f'v {world.patch_pattern} d', 
-            bias_shape='v d',
+            bias_shape=f'{world.token_pattern} d',
             d = network.dim, 
             **world.patch_sizes, **world.token_sizes
             )
@@ -32,19 +30,17 @@ class EinMask(torch.nn.Module):
         self.to_fields = EinMix(
             pattern=f"b {world.flat_token_pattern} d -> {world.field_pattern} e", 
             weight_shape=f'v d {world.patch_pattern} e', 
-            bias_shape='v e',
+            bias_shape=f'{world.token_pattern} {world.patch_pattern} e',
             d = network.dim, 
             e = default(network.num_tails, 1), 
             **world.patch_sizes, **world.token_sizes 
             )
         
+        self.to_queries = ContinuousPositionalEmbedding(network.dim_coords, network.wavelengths, model_dim= network.dim)
+
         # maybe context projection
         if default(network.dim_noise, 0) > 0:
             self.to_context = GatedFFN(network.dim_noise, bias= True)
-
-        # positional projections
-        self.to_positions = torch.nn.Linear(self.cpe.embedding_dim, network.dim)
-        self.to_queries = torch.nn.Linear(self.cpe.embedding_dim, network.dim)
 
         # latent transformer
         self.latents = torch.nn.Embedding(network.num_latents, network.dim)
@@ -84,7 +80,7 @@ class EinMask(torch.nn.Module):
         if isinstance(m, EinMix):
             torch.nn.init.trunc_normal_(m.weight, std = 0.02)
             if m.bias is not None:
-                torch.nn.init.zeros_(m.bias)
+                torch.nn.init.trunc_normal_(m.bias, std = 0.02)
         # conditional layer norm
         if isinstance(m, ConditionalLayerNorm):
             if m.bias is not None:
@@ -120,20 +116,19 @@ class EinMask(torch.nn.Module):
         return xs
     
     def _step(self, fields: torch.FloatTensor, visible: torch.LongTensor, noise: torch.FloatTensor) -> torch.FloatTensor:
-        # expand embeddings to batch size
-        B = fields.size(0)
-        latents = einops.repeat(self.latents.weight, 'm d -> b m d', b = B)
-        coords = einops.repeat(self.coordinates, "n d -> b n d", b = B)
-
-        # prepare tokens and queries        
-        tokens = self.to_positions(coords) + self.to_tokens(fields)
-        queries = self.to_queries(coords)
-
-        # maybe prepare context
-        context = self.to_context(noise) if exists(self.to_context) else None        
-
-        # only encode visible tokens
+        # project tokens and select visible        
+        tokens = self.to_tokens(fields)
         tokens = tokens.gather(1, visible)
+
+        # project queries and expand to batch size
+        queries = self.to_queries(self.coordinates)
+        queries = einops.repeat(queries, "n d -> b n d", b = tokens.size(0))
+
+        # expand latents to batch size
+        latents = einops.repeat(self.latents.weight, 'm d -> b m d', b = tokens.size(0))
+
+        # maybe project context
+        context = self.to_context(noise) if exists(self.to_context) else None        
 
         # apply flamingo-style transformer
         for block in self.encoder:
