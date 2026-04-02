1074f5afefc202862dec00b47987a05513c46181
jthuemmel <jannik.thuemmel@posteo.net>
2026-04-02 18:03:25 +0200

    Variant with Perceiver and Tails

--- utils/einmask.py
+++ utils/einmask.py
@@ -12,8 +12,10 @@ class EinMask(torch.nn.Module):
         self.world = world
 
         # learnable parameters
-        self.mask_token = torch.nn.Parameter(torch.zeros((network.dim_in,)))
-        self.position_codes = torch.nn.Parameter(self.init_sincos_positions(network.dim_in))
+        self.latent_tokens = torch.nn.Embedding(network.num_latents, network.dim_in)
+        self.mask_token = torch.nn.Embedding(2, network.dim_out)
+        self.tgt_positions = torch.nn.Parameter(self.init_sincos_positions(network.dim_out))
+        self.src_positions = torch.nn.Parameter(self.init_sincos_positions(network.dim_in))
 
         # I/O
         self.field_encoder = EinMix(
@@ -24,16 +26,15 @@ class EinMask(torch.nn.Module):
         self.field_decoder = FieldDecoder(network, world)
         
         # Encoder
-        self.noise_encoder = GatedFFN(network.dim_ctx)
         self.encoder = torch.nn.ModuleList([
               TransformerBlock(dim= network.dim_in, num_heads= network.num_encoder_heads) 
               for _ in range(default(network.num_read_blocks, 1))
               ])
         
         # Mask decoder
-        self.to_decoder = torch.nn.Linear(network.dim_in, network.dim_out, bias = False)
+        self.to_decoder = torch.nn.Sequential(torch.nn.Linear(network.dim_in, network.dim_out, bias = False), torch.nn.RMSNorm(network.dim_out))
         self.decoder = torch.nn.ModuleList([
-              TransformerBlock(dim= network.dim_out, dim_ctx = network.dim_ctx, num_heads= network.num_decoder_heads) 
+              TransformerBlock(dim= network.dim_out, num_heads= network.num_decoder_heads) 
               for _ in range(default(network.num_write_blocks, 1))
               ])
 
@@ -74,48 +75,25 @@ class EinMask(torch.nn.Module):
             torch.nn.init.trunc_normal_(m.weight, std = 0.02)
             if exists(m.bias):
                 torch.nn.init.zeros_(m.bias)
-        # adaLN
-        elif isinstance(m, AdaptiveLayerNorm):
-            torch.nn.init.zeros_(m.bias)
-            if exists(m.weight):
-                torch.nn.init.trunc_normal_(m.weight, std = 1e-5)
-        elif isinstance(m, EinMask):
-            torch.nn.init.trunc_normal_(m.mask_token, std = 0.02)
     
     def forward(self, 
                 fields: torch.FloatTensor, 
                 visible: torch.BoolTensor, 
-                members: int = 1,
-                rng: Optional[torch.Generator] = None,
+                **kwargs
                 ) -> torch.FloatTensor:
         B = visible.size(0)
-        # tokenize
-        tokens = self.field_encoder(fields) + self.position_codes
+        tokens = self.field_encoder(fields) + self.src_positions
 
-        # encode visible
         src = einops.rearrange(tokens[visible], '(b n) d -> b n d', b = B)
+        latents = einops.repeat(self.latent_tokens.weight, 'z d -> b z d', b = B)
         for read in self.encoder:
-            src = read(src)
-
-        # pad with mask tokens
-        tgt = (self.mask_token + self.position_codes).type_as(src)
-        tgt = torch.masked_scatter(
-            input = tgt, # for all masked locations
-            mask = visible[..., None], # where visible is True
-            source = src # copy src elements over
-            )
-    
-        # stochastic conditioning
-        tgt = einops.repeat(tgt, 'b n d -> (b e) n d', e = members)
-        noise = torch.randn([B * members, 1, self.network.dim_ctx], device = tgt.device, dtype = tgt.dtype, generator = rng)
-        noise = self.noise_encoder(noise)
-
-        # decode (all tokens | noise)
-        tgt = self.to_decoder(tgt)
+            latents = read(latents, kv = torch.cat([latents, src], dim = 1))
+            
+        latents = self.to_decoder(latents)
+        tgt = self.mask_token(visible.long()) + self.tgt_positions
         for write in self.decoder:
-            tgt = write(tgt, ctx = noise)
+            tgt = write(tgt, kv = torch.cat([latents, tgt], dim = 1))
 
-        # tokens -> field and ensemble -> last
         tgt = self.field_decoder(tgt)
         tgt = einops.rearrange(tgt, '(b e) ... -> b ... e', b = B)
         return tgt
